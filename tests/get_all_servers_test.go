c53083682d5d5283737100545a06e849ebc8e04d
Andre' Jenkins <andrej@ccbill.com>
2019-08-07 18:25:04 +0200

    Added full get-all test

--- tests/get_all_servers_test.go
+++ tests/get_all_servers_test.go
@@ -18,38 +18,88 @@ import (
 	"phoenixnap.com/pnap-cli/tests/generators"
 )
 
-func TestGetAllServersSuccess(test_framework *testing.T) {
+func TestGetAllServersShortSuccess(test_framework *testing.T) {
+	// init controller
 	controller := gomock.NewController(test_framework)
 
+	// mocks
 	mockClient := mocks.NewMockWebClient(controller)
 	mockPrinter := mocks.NewMockPrinter(controller)
 
+	// generate 3 long servers
 	serverlist := []servers.LongServer{
 		generators.GenerateServer(),
 		generators.GenerateServer(),
 		generators.GenerateServer(),
 	}
 
+	// marshal array into bytes
 	data, _ := json.Marshal(serverlist)
 
+	// manufacture response
 	resp := http.Response{
 		StatusCode: 200,
 		Body:       ioutil.NopCloser(bytes.NewBuffer(data)),
 	}
 
+	// swap Mains with Mocks
 	client.MainClient = mockClient
 	printer.MainPrinter = mockPrinter
 
+	// mock client
 	mockClient.
 		EXPECT().
-		PerformGet("servers").
-		Return(&resp, nil)
+		PerformGet("servers"). // should hit GET /servers
+		Return(&resp, nil)     // returns &resp and nil
 
 	mockPrinter.
 		EXPECT().
-		PrintOutput(data, &[]servers.ShortServer{}).
+		PrintOutput(data, &[]servers.ShortServer{}). // should print short server
 		Return(3, nil)
 
 	os.Args = []string{"pnapctl", "bmc", "get", "servers"}
+	pnapctl.Execute()
+}
+
+func TestGetAllServersLongSuccess(test_framework *testing.T) {
+	// init controller
+	controller := gomock.NewController(test_framework)
+
+	// mocks
+	mockClient := mocks.NewMockWebClient(controller)
+	mockPrinter := mocks.NewMockPrinter(controller)
+
+	// generate 3 long servers
+	serverlist := []servers.LongServer{
+		generators.GenerateServer(),
+		generators.GenerateServer(),
+		generators.GenerateServer(),
+	}
+
+	// marshal array into bytes
+	data, _ := json.Marshal(serverlist)
+
+	// manufacture response
+	resp := http.Response{
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(bytes.NewBuffer(data)),
+	}
+
+	// swap Mains with Mocks
+	client.MainClient = mockClient
+	printer.MainPrinter = mockPrinter
+
+	// mock client
+	mockClient.
+		EXPECT().
+		PerformGet("servers"). // should hit GET /servers
+		Return(&resp, nil)     // returns &resp and nil
+
+	mockPrinter.
+		EXPECT().
+		PrintOutput(data, &[]servers.LongServer{}). // should print short server
+		Return(3, nil)
+
+	os.Args = []string{"pnapctl", "bmc", "get", "servers", "--full"}
 	pnapctl.Execute()
 }
