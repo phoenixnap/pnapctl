ce41184c2e3a6f0af47cd60fcff90817de4f7709
Andre' Jenkins <andrej@ccbill.com>
2019-08-14 09:35:23 +0200

    Updated get-all tests

--- tests/get_all_servers_test.go
+++ tests/get_all_servers_test.go
@@ -1,23 +1,17 @@
 package tests
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
-	"io/ioutil"
-	"net/http"
 	"testing"
-
-	"github.com/golang/mock/gomock"
-	"phoenixnap.com/pnap-cli/pnapctl/client"
-	mocks "phoenixnap.com/pnap-cli/pnapctl/mocks"
-
-	"phoenixnap.com/pnap-cli/pnapctl/printer"
 
 	"phoenixnap.com/pnap-cli/pnapctl/bmc/get/servers"
 	"phoenixnap.com/pnap-cli/tests/generators"
 	. "phoenixnap.com/pnap-cli/tests/mockhelp"
 )
+
+func TestGetAllSetup(test_framework *testing.T) {
+	URL = "servers"
+}
 
 func TestGetAllServersShortSuccess(test_framework *testing.T) {
 	serverlist := generators.GenerateServers(3)
@@ -80,50 +74,23 @@ func TestGetAllServersClientFailure(test_framework *testing.T) {
 }
 
 func TestGetAllServersPrinterFailure(test_framework *testing.T) {
-	// init controller
-	controller := gomock.NewController(test_framework)
+	serverlist := generators.GenerateServers(3)
 
-	// mocks
-	mockClient := mocks.NewMockWebClient(controller)
-	mockPrinter := mocks.NewMockPrinter(controller)
+	// Mocking
+	PrepareMockClient(test_framework).
+		PerformGet(URL).
+		Return(WithResponse(200, WithBody(serverlist)), nil)
 
-	// generate 3 long servers
-	serverlist := []servers.LongServer{
-		generators.GenerateServer(),
-		generators.GenerateServer(),
-		generators.GenerateServer(),
-	}
-
-	// marshal array into bytes
-	data, _ := json.Marshal(serverlist)
-
-	// manufacture response
-	resp := http.Response{
-		StatusCode: 200,
-		Body:       ioutil.NopCloser(bytes.NewBuffer(data)),
-	}
-
-	// swap Mains with Mocks
-	client.MainClient = mockClient
-	printer.MainPrinter = mockPrinter
-
-	// mock client
-	mockClient.
-		EXPECT().
-		PerformGet("servers"). // should hit GET /servers
-		Return(&resp, nil)     // returns &resp and nil
-
-	mockPrinter.
-		EXPECT().
-		PrintOutput(data, &[]servers.LongServer{}). // should print short server
-		Return(-1, errors.New("test-error"))
+	PrepareMockPrinter(test_framework).
+		PrintOutput(WithData(serverlist), &[]servers.LongServer{}).
+		Return(3, errors.New("oops"))
 
 	// to display full output
 	servers.Full = true
 
 	err := servers.GetServersCmd.RunE(servers.GetServersCmd, []string{})
 
-	if err.Error() != "test-error" {
+	if err.Error() != "oops" {
 		test_framework.Error("Expected printer failure error, found:", err)
 	}
 }
