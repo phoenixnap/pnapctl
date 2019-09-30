867653591ffeb68421332424e8eeb045497d932c
Niki Micallef <nikim@ccbill.com>
2019-09-30 11:12:55 +0200

    - Changing Keycloak URL proxy to ApiGee proxy URL

--- pnapctl/configuration/properties.go
+++ pnapctl/configuration/properties.go
@@ -4,5 +4,5 @@ const (
 	// Hostname represents the URL entrypoint of our application
 	Hostname = "https://phoenixnap-non-prod-ph-dev.apigee.net/api/bmc/v0/"
 	// TokenURL represents the URL of the OpenID Connect provider from where we can retrieve a token
-	TokenURL = "https://kc.allbyvmself.com:8443/auth/realms/BMC/protocol/openid-connect/token"
+	TokenURL = "https://phoenixnap-non-prod-ph-dev.apigee.net/auth/realms/BMC/protocol/openid-connect/token"
 )
