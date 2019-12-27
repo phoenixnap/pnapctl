a8d7a5d9b2399094a0fd99649b1233fcea3335ba
Marko Djuricic <markodj@ccbill.com>
2019-12-27 13:42:13 +0000

    Feature/bmc 785 ur ls should be environment specific

--- pnapctl/configuration/properties_dev.go
+++ pnapctl/configuration/properties_dev.go
@@ -1,12 +1,12 @@
-// +build !dev
+// +build dev
 
 package configuration
 
 const (
 	// Hostname represents the URL entrypoint of our application
-	Hostname = "https://api.phoenixnap.com/bmc/v0/"
+	Hostname = "https://api-dev.phoenixnap.com/bmc/v0/"
 	// TokenURL represents the URL of the OpenID Connect provider from where we can retrieve a token
-	TokenURL = "https://auth.phoenixnap.com/auth/realms/BMC/protocol/openid-connect/token"
+	TokenURL = "https://auth-dev.phoenixnap.com/auth/realms/BMC/protocol/openid-connect/token"
 	// KnowledgeBaseURL represents the URL of the public knowledge base for pnapCTL
-	KnowledgeBaseURL = "https://developers.phoenixnap.com/cli"
+	KnowledgeBaseURL = "https://developers-dev.phoenixnap.com/cli"
 )
