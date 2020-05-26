4d3aa9309019af286fb92e1b9f51f116bf6be9bf
Maja Kovacevic <majak@ccbill.com>
2020-05-26 16:10:50 +0200

    Change server type 's1.c1.tiny' to 's1.c1.small' in 'create server' command description.

--- commands/create/server/createserver.go
+++ commands/create/server/createserver.go
@@ -48,7 +48,7 @@ pnapctl create server --filename ~/server.yaml
 hostname: "new-server"
 description: "New server description"
 os: "ubuntu/bionic"
-type: "s1.c1.tiny"
+type: "s1.c1.small"
 location: "PHX"
 sshKeys:
 	- "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAklOUpkDHrfHY17SbrmTIpNLTGK9Tjom/BWDSUGPl+nafzlHDTYW7hdI4yZ5ew18JH4JW9jbhUFrviQzM7xlELEVf4h9lFX5QVkbPppSwg0cda3Pbv7kOdJ/MTyBlWXFCR+HAo3FXRitBqxiX1nKhXpHAZsMciLq8V6RjsNAQwdsdMFvSlVK/7XAt3FaoJoAsncM1Q9x5+3V0Ww68/eIFmb1zuUFljQJKprrX88XypNDvjYNby6vw/Pb0rwert/EnmZ+AW4OZPnTPI89ZPmVMLuayrD2cE86Z/il8b+gw3r3+1nKatmIkjn2so1d01QraTlMqVSsbxNrRFi9wrf+M7Q== test1@test"
