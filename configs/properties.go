533c90c8e2c1eb951247fc4e3dd529446ac8c535
Maja Kovacevic <majak@ccbill.com>
2020-06-05 13:18:59 +0200

    Update API version to 'v1beta'.

--- configs/properties.go
+++ configs/properties.go
@@ -4,7 +4,7 @@ package configuration
 
 const (
 	// Hostname represents the URL entrypoint of our application
-	Hostname = "https://api.phoenixnap.com/bmc/v0/"
+	Hostname = "https://api.phoenixnap.com/bmc/v1beta/"
 	// TokenURL represents the URL of the OpenID Connect provider from where we can retrieve a token
 	TokenURL = "https://auth.phoenixnap.com/auth/realms/BMC/protocol/openid-connect/token"
 	// KnowledgeBaseURL represents the URL of the public knowledge base for pnapCTL
