d57ef96ff91b5d548818200f60319a09b3c8b179
Andrea Jenkins <mctech26@gmail.com>
2021-11-12 09:07:37 +0000

    Feature/bmc 6485/cover bmc audit endpoints

---
+++ common/models/tables/eventtable.go
@@ -0,0 +1,22 @@
+package tables
+
+import (
+	"time"
+
+	auditsdk "gitlab.com/phoenixnap/bare-metal-cloud/go-sdk.git/auditapi"
+	auditmodels "phoenixnap.com/pnap-cli/common/models/auditmodels"
+)
+
+type Event struct {
+	Name      *string   `header:"Name"`
+	Timestamp time.Time `header:"Timestamp"`
+	UserInfo  string    `header:"User Info"`
+}
+
+func ToEventTable(event auditsdk.Event) Event {
+	return Event{
+		Name:      event.Name,
+		Timestamp: event.Timestamp,
+		UserInfo:  auditmodels.UserInfoToTableString(&event.UserInfo),
+	}
+}
---
+++ tests/generators/eventgenerator.go
@@ -0,0 +1,27 @@
+package generators
+
+import (
+	"time"
+
+	auditapisdk "gitlab.com/phoenixnap/bare-metal-cloud/go-sdk.git/auditapi"
+)
+
+func GenerateEvent() auditapisdk.Event {
+	return auditapisdk.Event{
+		Name:      randSeqPointer(10),
+		Timestamp: time.Now(),
+		UserInfo: auditapisdk.UserInfo{
+			AccountId: randSeq(10),
+			ClientId:  randSeqPointer(10),
+			Username:  randSeq(10),
+		},
+	}
+}
+
+func GenerateEvents(n int) []auditapisdk.Event {
+	var eventList []auditapisdk.Event
+	for i := 0; i < n; i++ {
+		eventList = append(eventList, GenerateEvent())
+	}
+	return eventList
+}
