18967d4ec3d9db2ded028c0e6c8c772e0f1672ec
andrewbu <andrewbu@ccbill.com>
2021-11-17 13:37:10 +0100

    updating reset to mark filename as optional. updating readme

--- commands/reset/server/resetserver.go
+++ commands/reset/server/resetserver.go
@@ -7,6 +7,7 @@ import (
 	"phoenixnap.com/pnap-cli/common/ctlerrors"
 	"phoenixnap.com/pnap-cli/common/models/bmcapimodels"
 
+	bmcapisdk "github.com/phoenixnap/go-sdk-bmc/bmcapi"
 	"github.com/spf13/cobra"
 )
 
@@ -32,7 +33,7 @@ sshKeys:
 	- "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAklOUpkDHrfHY17SbrmTIpNLTGK9Tjom/BWDSUGPl+nafzlHDTYW7hdI4yZ5ew18JH4JW9jbhUFrviQzM7xlELEVf4h9lFX5QVkbPppSwg0cda3Pbv7kOdJ/MTyBlWXFCR+HAo3FXRitBqxiX1nKhXpHAZsMciLq8V6RjsNAQwdsdMFvSlVK/7XAt3FaoJoAsncM1Q9x5+3V0Ww68/eIFmb1zuUFljQJKprrX88XypNDvjYNby6vw/Pb0rwert/EnmZ+AW4OZPnTPI89ZPmVMLuayrD2cE86Z/il8b+gw3r3+1nKatmIkjn2so1d01QraTlMqVSsbxNrRFi9wrf+M7Q== test1@test"
 	- "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyVGaw1PuEl98f4/7Kq3O9ZIvDw2OFOSXAFVqilSFNkHlefm1iMtPeqsIBp2t9cbGUf55xNDULz/bD/4BCV43yZ5lh0cUYuXALg9NI29ui7PEGReXjSpNwUD6ceN/78YOK41KAcecq+SS0bJ4b4amKZIJG3JWmDKljtv1dmSBCrTmEAQaOorxqGGBYmZS7NQumRe4lav5r6wOs8OACMANE1ejkeZsGFzJFNqvr5DuHdDL5FAudW23me3BDmrM9ifUzzjl1Jwku3bnRaCcjaxH8oTumt1a00mWci/1qUlaVFft085yvVq7KZbF2OPPbl+erDW91+EZ2FgEi+v1/CSJ5 test2@test"`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		resetRequest, err := bmcapimodels.CreateResetRequestFromFile(Filename, commandName)
+		resetRequest, err := createResetRequest()
 
 		if err != nil {
 			return err
@@ -54,6 +55,14 @@ sshKeys:
 }
 
 func init() {
+	//filename is optional
 	ResetServerCmd.Flags().StringVarP(&Filename, "filename", "f", "", "File containing required information for reset")
-	ResetServerCmd.MarkFlagRequired("filename")
+}
+
+func createResetRequest() (*bmcapisdk.ServerReset, error) {
+	if len(Filename) < 1 {
+		return &bmcapisdk.ServerReset{}, nil
+	} else {
+		return bmcapimodels.CreateResetRequestFromFile(Filename, commandName)
+	}
 }
