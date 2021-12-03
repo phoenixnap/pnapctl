dd094af64e2edc884611196949b510eb762ed533
Brian Zammit <brianz@ccbill.com>
2021-12-03 11:48:47 +0100

    Adding file

---
+++ src/common/models/auditmodels/testutil_eventgenerator.go
@@ -0,0 +1,46 @@
+package auditmodels
+
+import (
+	"time"
+
+	auditapisdk "github.com/phoenixnap/go-sdk-bmc/auditapi"
+	"phoenixnap.com/pnapctl/tests/generators"
+)
+
+func GenerateEvent() auditapisdk.Event {
+	return auditapisdk.Event{
+		Name:      generators.RandSeqPointer(10),
+		Timestamp: time.Now(),
+		UserInfo: auditapisdk.UserInfo{
+			AccountId: generators.RandSeq(10),
+			ClientId:  generators.RandSeqPointer(10),
+			Username:  generators.RandSeq(10),
+		},
+	}
+}
+
+func GenerateEvents(n int) []auditapisdk.Event {
+	var eventList []auditapisdk.Event
+	for i := 0; i < n; i++ {
+		eventList = append(eventList, GenerateEvent())
+	}
+	return eventList
+}
+
+func GenerateQueryParams() EventsGetQueryParams {
+	now := reprocessTime(time.Now())
+	return EventsGetQueryParams{
+		From:     &now,
+		To:       &now,
+		Limit:    10,
+		Order:    "ASC",
+		Username: generators.RandSeq(10),
+		Verb:     "PUT",
+		Uri:      generators.RandSeq(10),
+	}
+}
+
+func reprocessTime(t time.Time) time.Time {
+	result, _ := time.Parse(time.RFC3339, t.Format(time.RFC3339))
+	return result
+}
