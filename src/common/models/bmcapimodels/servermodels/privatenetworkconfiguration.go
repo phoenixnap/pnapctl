1ed6c12306a39cd33a2ed97ef596cbc937fed12f
Brian Zammit <brianz@ccbill.com>
2021-12-06 17:23:28 +0100

    Adding untracked files

---
+++ src/common/models/bmcapimodels/servermodels/privatenetworkconfiguration.go
@@ -0,0 +1,35 @@
+package servermodels
+
+import (
+	bmcapisdk "github.com/phoenixnap/go-sdk-bmc/bmcapi"
+)
+
+type PrivateNetworkConfiguration struct {
+	GatewayAddress    *string                 `yaml:"gatewayAddress" json:"gatewayAddress"`
+	ConfigurationType *string                 `yaml:"configurationType" json:"configurationType"`
+	PrivateNetworks   *[]ServerPrivateNetwork `yaml:"privateNetworks" json:"privateNetworks"`
+}
+
+func (privateNetConf *PrivateNetworkConfiguration) toSdk() *bmcapisdk.PrivateNetworkConfiguration {
+	if privateNetConf == nil {
+		return nil
+	}
+
+	return &bmcapisdk.PrivateNetworkConfiguration{
+		GatewayAddress:    privateNetConf.GatewayAddress,
+		ConfigurationType: privateNetConf.ConfigurationType,
+		PrivateNetworks:   mapServerPrivateNetworksToSdk(privateNetConf.PrivateNetworks),
+	}
+}
+
+func privateNetworkConfigurationFromSdk(privateNetworkConfnfiguration *bmcapisdk.PrivateNetworkConfiguration) *PrivateNetworkConfiguration {
+	if privateNetworkConfnfiguration == nil {
+		return nil
+	}
+
+	return &PrivateNetworkConfiguration{
+		GatewayAddress:    privateNetworkConfnfiguration.GatewayAddress,
+		ConfigurationType: privateNetworkConfnfiguration.ConfigurationType,
+		PrivateNetworks:   privateNetworksFromSdk(privateNetworkConfnfiguration.PrivateNetworks),
+	}
+}
