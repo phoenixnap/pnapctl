fbe1a1ebfd614a3d0e622738a0204cd9c4fbebef
Brian Zammit <brianz@ccbill.com>
2021-12-15 11:31:49 +0100

    Adding OsConfigurationMap model test

---
+++ src/common/models/bmcapimodels/servermodels/osconfigurationmap_test.go
@@ -0,0 +1,43 @@
+package servermodels
+
+import (
+	"testing"
+
+	bmcapisdk "github.com/phoenixnap/go-sdk-bmc/bmcapi"
+	"phoenixnap.com/pnapctl/testsupport/testutil"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tests
+func TestOsConfigurationMapToSdk(test_framework *testing.T) {
+	cliModel := GenerateOsConfigurationMapCli()
+	sdkModel := OsConfigurationMapToSDK(&cliModel)
+
+	assertEqualOsConfigurationMap(test_framework, cliModel, *sdkModel)
+}
+
+func TestEmptyOsConfigurationMapToSdk(test_framework *testing.T) {
+	cliModel := &OsConfigurationMap{}
+	sdkModel := OsConfigurationMapToSDK(cliModel)
+
+	assertEqualOsConfigurationMap(test_framework, *cliModel, *sdkModel)
+}
+
+func TestNilOsConfigurationMapToSdk(test_framework *testing.T) {
+	var cliModel *OsConfigurationMap = nil
+
+	assert.Nil(test_framework, OsConfigurationMapToSDK(cliModel))
+}
+
+// assertion functions
+func assertEqualOsConfigurationMap(test_framework *testing.T, cliOsConfigurationMap OsConfigurationMap, sdkOsConfigurationMap bmcapisdk.OsConfigurationMap) {
+
+	if testutil.AssertNilEquality(test_framework, "Windows Configuration", cliOsConfigurationMap.Windows, sdkOsConfigurationMap.Windows) {
+		assertEqualOsConfigurationWindows(test_framework, *cliOsConfigurationMap.Windows, *sdkOsConfigurationMap.Windows)
+	}
+
+	if testutil.AssertNilEquality(test_framework, "Esxi Configuration", cliOsConfigurationMap.Esxi, sdkOsConfigurationMap.Esxi) {
+		assertEqualOsConfigurationMapEsxi(test_framework, *cliOsConfigurationMap.Esxi, *sdkOsConfigurationMap.Esxi)
+	}
+}
---
+++ src/common/models/bmcapimodels/servermodels/osconfigurationmapesxi_test.go
@@ -0,0 +1,46 @@
+package servermodels
+
+import (
+	"testing"
+
+	bmcapisdk "github.com/phoenixnap/go-sdk-bmc/bmcapi"
+	"phoenixnap.com/pnapctl/testsupport/testutil"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// tests
+func TestOsConfigurationMapEsxiToSdk(test_framework *testing.T) {
+	cliModel := GenerateOsConfigurationMapEsxiCli()
+	sdkModel := cliModel.toSdk()
+
+	assertEqualOsConfigurationMapEsxi(test_framework, *cliModel, *sdkModel)
+}
+
+func TestEmptyOsConfigurationMapEsxiToSdk(test_framework *testing.T) {
+	cliModel := &OsConfigurationMapEsxi{}
+	sdkModel := cliModel.toSdk()
+
+	assertEqualOsConfigurationMapEsxi(test_framework, *cliModel, *sdkModel)
+}
+
+func TestNilOsConfigurationMapEsxiToSdk(test_framework *testing.T) {
+	var cliModel *OsConfigurationMapEsxi = nil
+
+	assert.Nil(test_framework, cliModel.toSdk())
+}
+
+// assertion functions
+func assertEqualOsConfigurationMapEsxi(test_framework *testing.T, cliOsConfigurationMapEsxi OsConfigurationMapEsxi, sdkOsConfigurationMapEsxi bmcapisdk.OsConfigurationMapEsxi) {
+
+	assert.Equal(test_framework, cliOsConfigurationMapEsxi.RootPassword, sdkOsConfigurationMapEsxi.RootPassword)
+	assert.Equal(test_framework, cliOsConfigurationMapEsxi.ManagementUiUrl, sdkOsConfigurationMapEsxi.ManagementUiUrl)
+
+	if testutil.AssertNilEquality(test_framework, "Management Access Allowed IPs", cliOsConfigurationMapEsxi.ManagementAccessAllowedIps, sdkOsConfigurationMapEsxi.ManagementAccessAllowedIps) {
+		assert.Equal(test_framework, len(*cliOsConfigurationMapEsxi.ManagementAccessAllowedIps), len(*sdkOsConfigurationMapEsxi.ManagementAccessAllowedIps))
+
+		for i := range *cliOsConfigurationMapEsxi.ManagementAccessAllowedIps {
+			assert.Equal(test_framework, (*cliOsConfigurationMapEsxi.ManagementAccessAllowedIps)[i], (*sdkOsConfigurationMapEsxi.ManagementAccessAllowedIps)[i])
+		}
+	}
+}
