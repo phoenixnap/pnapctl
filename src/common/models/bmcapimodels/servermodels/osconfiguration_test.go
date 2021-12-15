7a55bcc18a832f4603715e9bb90afb9562c354ff
Brian Zammit <brianz@ccbill.com>
2021-12-15 10:08:19 +0100

    Finalizing Os Configuration model tests

--- src/common/models/bmcapimodels/servermodels/osconfiguration_test.go
+++ src/common/models/bmcapimodels/servermodels/osconfiguration_test.go
@@ -30,6 +30,26 @@ func TestNilOsConfigurationToSdk(test_framework *testing.T) {
 	assert.Nil(test_framework, cliModel.toSdk())
 }
 
+func TestOsConfigurationFromSdk(test_framework *testing.T) {
+	sdkModel := GenerateOsConfigurationSdk()
+	cliModel := OsConfigurationFromSdk(&sdkModel)
+
+	assertEqualOsConfiguration(test_framework, *cliModel, sdkModel)
+}
+
+func TestEmptyOsConfigurationFromSdk(test_framework *testing.T) {
+	var sdkModel *bmcapisdk.OsConfiguration = &bmcapisdk.OsConfiguration{}
+	cliModel := OsConfigurationFromSdk(sdkModel)
+
+	assertEqualOsConfiguration(test_framework, *cliModel, *sdkModel)
+}
+
+func TestNilOsConfigurationFromSdk(test_framework *testing.T) {
+	var sdkModel *bmcapisdk.OsConfiguration = nil
+
+	assert.Nil(test_framework, OsConfigurationFromSdk(sdkModel))
+}
+
 // assertion functions
 func assertEqualOsConfiguration(test_framework *testing.T, cliOsConfiguration OsConfiguration, sdkOsConfiguration bmcapisdk.OsConfiguration) {
 
