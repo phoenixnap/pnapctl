d217f2c71f4813b6965a416cdc5abe8c70e73b84
andrej <andrej@ccbill.com>
2022-10-19 13:13:48 +0200

    Removed unused assertion

--- src/testsupport/testutil/assertions.go
+++ src/testsupport/testutil/assertions.go
@@ -2,23 +2,10 @@ package testutil
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func AssertNilEquality(test_framework *testing.T, varName string, cliVar interface{}, sdkVar interface{}) bool {
-	if cliVar == nil || reflect.ValueOf(cliVar).IsNil() {
-		assert.Nil(test_framework, sdkVar, "(value: "+varName+") CLI's value is nil, but not SDK's value.")
-		return false
-	} else if sdkVar == nil || reflect.ValueOf(sdkVar).IsNil() {
-		assert.Nil(test_framework, cliVar, "(value: "+varName+") SDK's value is nil, but not CLI's value.")
-		return false
-	}
-
-	return true
-}
 
 func AssertIsType[T any](test_framework *testing.T, item interface{}) {
 	var t T
