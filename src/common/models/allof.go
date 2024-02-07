f44f0c203234260c750f0b4bff74b06683831920
YandrickG <yandrickg@ccbill.com>
2024-02-07 08:55:27 +0100

    Updating BillingApi to v2.0.0

--- src/common/models/allof.go
+++ src/common/models/allof.go
@@ -3,6 +3,7 @@ package models
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/rs/zerolog/log"
 )
 
 func typename[A any]() string {
@@ -15,16 +16,14 @@ func GetFromAllOf[Inner any, AllOf any](allOf AllOf) *Inner {
 	bin, err := json.Marshal(allOf)
 
 	if err != nil {
-		// TODO replace with log
-		// fmt.Printf("Error when extracting JSON from (%s): %v\n", typename[AllOf](), err)
+		log.Err(err).Msg(fmt.Sprintf("Error when extracting JSON from (%s)\n", typename[AllOf]()))
 		return nil
 	}
 
 	err = json.Unmarshal(bin, &inner)
 
 	if err != nil {
-		// TODO replace with log
-		// fmt.Printf("Error when parsing (%s) from (%s): %v\n", typename[Inner](), typename[AllOf](), err)
+		log.Err(err).Msg(fmt.Sprintf("Error when parsing (%s) from (%s)\n", typename[Inner](), typename[AllOf]()))
 		return nil
 	}
 
