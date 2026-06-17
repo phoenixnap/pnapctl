f32ffac76c4debc0c644ffbab5148bb259d8c526
darrenccbill <93383166+darrenccbill@users.noreply.github.com>
2026-06-17 19:26:11 +0200

    Update server.go

--- src/commands/update/server/server.go
+++ src/commands/update/server/server.go
@@ -11,8 +11,8 @@ import (
 // UpdateServerCmd is the parent command for server update operations.
 var UpdateServerCmd = &cobra.Command{
 	Use:   "server",
-	Short: "Update a server's resources.",
-	Long:  `Update a server's resources.`,
+	Short: "Update a server.",
+	Long:  `Update a server.`,
 	Run: func(cmd *cobra.Command, _ []string) {
 		cmd.Help()
 		os.Exit(0)
